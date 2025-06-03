c30234333bab3a9b0e4c30d7ad5556ba24079912
HMQui <huynhqui6425@gmail.com>
2025-06-03 16:04:34 +0700

    fix bug db

--- src/models/configDB.js
+++ src/models/configDB.js
@@ -3,13 +3,12 @@ require("dotenv").config();
 
 const pool = new Pool({
     connectionString: process.env.URL_CONNECT_TO_SUPABASE,
-    ssl: process.env.URL_CONNECT_TO_SUPABASE.includes("localhost") ? false : { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false }
 });
 
 pool.on("error", (err) => {
     console.error("Unexpected error on idle PostgreSQL client", err);
 });
-
 
 console.log("Database pool has been initialized!");
 
