f6cd8b9baab677bf780c5b98f187e17e6afbcf6b
HMQui <huynhqui6425@gmail.com>
2025-06-03 21:00:05 +0700

    config cors for socket

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,14 +1,14 @@
 const { Server } = require("socket.io");
 
 const { verifySignUp } = require("../utils/sockets/verifySignUp");
-const { handleWithdrawSocket } = require("../utils/sockets/withdrawSocket")
+const { handleWithdrawSocket } = require("../utils/sockets/withdrawSocket");
 
 let io;
 
 const setupSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: process.env.CLIENT_URL,
+            origin: ["http://localhost:3000", "https://gofund.io.vn", "https://www.gofund.io.vn"],
             credentials: true,
             methods: ["GET", "POST", "PATCH", "DELETE"],
             allowedHeaders: ["Content-Type", "Authorization"]
